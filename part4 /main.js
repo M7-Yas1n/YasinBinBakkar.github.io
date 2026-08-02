8829a09389de1560615a95d24ebac1d3f172ce1e
Yasin Bin Bakkar <yasin.binbakkar@dcmail.ca>
2026-08-02 16:59:04 -0400

    Update main.js
    Signed-off-by: Yasin Bin Bakkar <yasin.binbakkar@dcmail.ca>

--- part4 /main.js
+++ part4 /main.js
@@ -72,21 +72,21 @@ class EvilCircle extends Shape {
     this.size = 10;
 
     window.addEventListener("keydown", (e) => {
-      switch (e.key) {
-        case "a":
-          this.x -= this.velX;
-          break;
-        case "d":
-          this.x += this.velX;
-          break;
-        case "w":
-          this.y -= this.velY;
-          break;
-        case "s":
-          this.y += this.velY;
-          break;
-      }
-    });
+  switch (e.key) {
+    case "a":
+      this.x -= this.velX;
+      break;
+    case "d":
+      this.x += this.velX;
+      break;
+    case "w":
+      this.y -= this.velY;
+      break;
+    case "s":
+      this.y += this.velY;
+      break;
+  }
+});
   }
 
   draw() {
