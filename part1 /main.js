21282e830357f7711d2e33528c9df8859032a680
Yasin Bin Bakkar <yasin.binbakkar@dcmail.ca>
2026-08-02 16:50:05 -0400

    Update main.js
    Signed-off-by: Yasin Bin Bakkar <yasin.binbakkar@dcmail.ca>

--- part1 /main.js
+++ part1 /main.js
@@ -5,53 +5,54 @@ Date: 31 July 2026
 Builds a random silly story from arrays of words and user input.
 */
 
-const storyText =
-  "It was 94 fahrenheit outside, so :insertx: and Bob went for a walk to :inserty:. " +
-  "When they arrived, they stared in amazement at :insertz:, which weighed exactly 300lb. " +
-  "Bob could not believe his eyes and decided to tell everyone he knew.";
+// Complete variable definitions and random functions
 
-const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-const insertY = ["the soup kitchen", "Disneyland", "the White House"];
-const insertZ = ["exploding tacos", "giant snowballs", "sushi burritos"];
-
-const customName = document.getElementById("customname");
-const randomize = document.querySelector("#generate");
-const story = document.querySelector("#story");
-
-randomize.addEventListener("click", result);
+const customName = document.getElementById("custom-name");
+const generateBtn = document.querySelector(".generate");
+const story = document.querySelector(".story");
 
 function randomValueFromArray(array) {
   const random = Math.floor(Math.random() * array.length);
   return array[random];
 }
 
-function result(e) {
-  e.preventDefault();
+// Raw text strings
 
-  let newStory = storyText;
+// Willy the Goblin
+// Big Daddy
+// Father Christmas
 
-  const xItem = randomValueFromArray(insertX);
-  const yItem = randomValueFromArray(insertY);
-  const zItem = randomValueFromArray(insertZ);
+// the soup kitchen
+// Disneyland
+// the White House
 
-  newStory = newStory.replaceAll(":insertx:", xItem);
-  newStory = newStory.replaceAll(":inserty:", yItem);
-  newStory = newStory.replaceAll(":insertz:", zItem);
+// spontaneously combusted
+// melted into a puddle on the sidewalk
+// turned into a slug and slithered away
 
+// Partial return random string function
+
+function returnRandomStoryString() {
+  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+
+  return storyText;
+}
+
+// Event listener and partial generate function definition
+
+generateBtn.addEventListener("click", generateStory);
+
+function generateStory() {
   if (customName.value !== "") {
     const name = customName.value;
-    newStory = newStory.replaceAll("Bob", name);
   }
 
   if (document.getElementById("uk").checked) {
-    const weightNumber = 300;
-    const weightMetric = Math.round(weightNumber / 14) + " stone";
-    newStory = newStory.replace("300lb", weightMetric);
-
-    const tempNumber = 94;
-    const tempMetric = Math.round(((tempNumber - 32) * 5) / 9);
-    newStory = newStory.replace("94 fahrenheit", tempMetric + " centigrade");
+    const weight = Math.round(300);
+    const temperature = Math.round(94);
   }
 
-  story.textContent = newStory;
+  // TODO: replace "" with the correct expression
+  story.textContent = "";
+  story.style.visibility = "visible";
 }
